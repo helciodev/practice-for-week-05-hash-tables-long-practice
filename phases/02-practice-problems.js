7b74a83ae31bf06be6e0ed62bc233b31c55ea13d
helcio <h.andre@giap.pl>
2024-01-04 14:58:30 +0100

    Fix twoSum practice problem

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,26 +1,62 @@
 function anagrams(str1, str2) {
   // Your code here
-}
+  // const charsAsKeysObj = str1.split("").reduce((obj, currentChar) => {
+  //   if (!obj[currentChar]) obj[currentChar] = currentChar;
+  //   return obj;
+  // }, {});
 
+  // for (char of str2) {
+  //   if (!charsAsKeysObj[char]) return false;
+  // }
+  // return true;
+
+  const set = new Set(str1);
+
+  for (let char of str2) {
+    if (!set.has(char)) return false;
+  }
+  return true;
+}
 
 function commonElements(arr1, arr2) {
-  // Your code here
-}
+  const elementsSet = new Set(arr1);
 
+  return arr2.filter((el) => elementsSet.has(el));
+}
 
 function duplicate(arr) {
-  // Your code here
-}
+  let objWithElementsCount = {};
 
+  for (let el of arr) {
+    if (!objWithElementsCount[el]) {
+      objWithElementsCount[el] = 1;
+    } else {
+      objWithElementsCount[el]++;
+    }
+  }
+
+  for (let el of arr) {
+    if (objWithElementsCount[el] > 1) {
+      return el;
+    }
+  }
+  return "no duplicates";
+}
 
 function twoSum(nums, target) {
-  // Your code here
-}
+  const numsSet = new Set(nums);
 
+  for (let num of nums) {
+    let numToSearch = target - num;
+    numsSet.delete(num);
+    if (numsSet.has(numToSearch)) return true;
+    numsSet.add(num);
+  }
+  return false;
+}
 
 function wordPattern(pattern, strings) {
   // Your code here
 }
-
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
